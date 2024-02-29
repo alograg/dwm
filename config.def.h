f2f13dbfa89e48e46a50d94ab2835e3c03f7f0c9
alograg <alograg@gmail.com>
2024-02-29 10:22:12 +0100

    feat(patch): :lipstick: Whide tile layout

--- config.def.h
+++ config.def.h
@@ -45,6 +45,7 @@ static const Layout layouts[] = {
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 	{ "|+|",      tatami },
+	{ "[][]=",    tilewide },
 };
 
 /* key definitions */
@@ -81,6 +82,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_y,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY,                       XK_w,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
