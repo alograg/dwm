d26c6e86512debe0028332c8f21086b8fdfa4d6d
alograg <alograg@gmail.com>
2024-02-29 11:37:32 +0100

    feat(patch): :wheelchair: Shutdown and reboot keys

--- config.def.h
+++ config.def.h
@@ -64,6 +64,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *rebootcmd[]  = { "sudo", "shutdown", "-r", "+0", NULL };
+static const char *shutdowncmd[]  = { "sudo", "shutdown", "+0", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
@@ -103,6 +105,8 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_End,    spawn,          {.v = shutdowncmd} },
+	{ MODKEY|ShiftMask,             XK_Delete, spawn,          {.v = rebootcmd} },
 };
 
 /* button definitions */
